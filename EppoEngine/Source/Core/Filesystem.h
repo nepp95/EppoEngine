090063c4cd801a39547fecf964e5c560f7a36c6a
Niels Eppenhof <n.eppenhof@gmail.com>
2023-10-31 21:38:00 +0100

    Added WriteText and ReadText to Filesystem class to read/write text files from disk

--- EppoEngine/Source/Core/Filesystem.h
+++ EppoEngine/Source/Core/Filesystem.h
@@ -18,8 +18,11 @@ namespace Eppo
 		static bool Exists(const std::filesystem::path& path);
 
 		static Buffer ReadBytes(const std::filesystem::path& filepath);
+		static std::string ReadText(const std::filesystem::path& filepath);
+
 		static void WriteBytes(const std::filesystem::path& filepath, Buffer buffer, bool overwrite = true);
 		// Specifically used for shaders which are 4 byte aligned
 		static void WriteBytes(const std::filesystem::path& filepath, const std::vector<uint32_t>& buffer, bool overwrite = true);
+		static void WriteText(const std::filesystem::path& filepath, const std::string& text, bool overwrite = true);
 	};
 }
