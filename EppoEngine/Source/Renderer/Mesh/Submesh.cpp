d46322d6d9cd8024b0e9e3df02cd1af6fad8d775
Niels Eppenhof <n.eppenhof@gmail.com>
2024-12-19 17:35:32 +0100

    Remove redundant cast

--- EppoEngine/Source/Renderer/Mesh/Submesh.cpp
+++ EppoEngine/Source/Renderer/Mesh/Submesh.cpp
@@ -8,7 +8,7 @@ namespace Eppo
 	Submesh::Submesh(std::string name, const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices, const std::vector<Primitive>& primitives, const glm::mat4& transform)
 		: m_Primitives(primitives), m_Name(std::move(name)), m_LocalTransform(transform)
 	{
-		m_VertexBuffer = VertexBuffer::Create((void*)vertices.data(), vertices.size() * sizeof(Vertex));
-		m_IndexBuffer = IndexBuffer::Create((void*)indices.data(), indices.size() * sizeof(uint32_t));
+		m_VertexBuffer = VertexBuffer::Create(vertices.data(), vertices.size() * sizeof(Vertex));
+		m_IndexBuffer = IndexBuffer::Create(indices.data(), indices.size() * sizeof(uint32_t));
 	}
 }
