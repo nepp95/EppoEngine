582c876d5fc75b34eb415f50d903fba567b3d23e
Niels <n.eppenhof@gmail.com>
2026-07-07 20:40:04 +0000

    Fixed possible null reference in ScriptInstance

--- EppoEngine/Source/Scripting/ScriptInstance.cpp
+++ EppoEngine/Source/Scripting/ScriptInstance.cpp
@@ -9,26 +9,41 @@ namespace Eppo
 
     auto ScriptInstance::InvokeOnCreate() -> void
     {
-        m_Assembly->InvokeOnCreate(static_cast<uint64_t>(m_EntityId));
+        if (m_Assembly)
+            m_Assembly->InvokeOnCreate(static_cast<uint64_t>(m_EntityId));
+        else
+            Log::Warn("Trying ScriptInstance::InvokeOnCreate but assembly is null!");
     }
 
     auto ScriptInstance::InvokeOnUpdate(const float timestep) -> void
     {
-        m_Assembly->InvokeOnUpdate(static_cast<uint64_t>(m_EntityId), timestep);
+        if (m_Assembly)
+            m_Assembly->InvokeOnUpdate(static_cast<uint64_t>(m_EntityId), timestep);
+        else
+            Log::Warn("Trying ScriptInstance::InvokeOnUpdate but assembly is null!");
     }
 
     auto ScriptInstance::InvokeOnDestroy() -> void
     {
-        m_Assembly->InvokeOnDestroy(static_cast<uint64_t>(m_EntityId));
+        if (m_Assembly)
+            m_Assembly->InvokeOnDestroy(static_cast<uint64_t>(m_EntityId));
+        else
+            Log::Warn("Trying ScriptInstance::InvokeOnDestroy but assembly is null!");
     }
 
     auto ScriptInstance::GetFieldValue(const int32_t fieldIndex, void* data) const -> void
     {
-        m_Assembly->GetFieldValue(static_cast<uint64_t>(m_EntityId), fieldIndex, data);
+        if (m_Assembly)
+            m_Assembly->GetFieldValue(static_cast<uint64_t>(m_EntityId), fieldIndex, data);
+        else
+            Log::Warn("Trying ScriptInstance::GetFieldValue but assembly is null!");
     }
 
     auto ScriptInstance::SetFieldValue(const int32_t fieldIndex, const void* data) -> void
     {
-        m_Assembly->SetFieldValue(static_cast<uint64_t>(m_EntityId), fieldIndex, data);
+        if (m_Assembly)
+            m_Assembly->SetFieldValue(static_cast<uint64_t>(m_EntityId), fieldIndex, data);
+        else
+            Log::Warn("Trying ScriptInstance::SetFieldValue but assembly is null!");
     }
 }
