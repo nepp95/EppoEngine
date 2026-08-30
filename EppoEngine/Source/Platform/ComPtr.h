6d927e8f5df9174c2501b7f96ce1275517fd54fe
Niels Eppenhof <n.eppenhof@gmail.com>
2026-08-30 03:36:45 +0200

    Linux fix for ComPtr

--- EppoEngine/Source/Platform/ComPtr.h
+++ EppoEngine/Source/Platform/ComPtr.h
@@ -8,6 +8,32 @@
 #else
     #include <dxc/WinAdapter.h>
     template<typename T>
-    using ComPtr = CComPtr<T>;
+    class ComPtr : public CComPtr<T>
+    {
+        using Base = CComPtr<T>;
+    public:
+        using Base::Base;
+        using Base::operator=;
+
+        ComPtr() noexcept = default;
+
+        T* Get() const noexcept { return this->p; }
+
+        T* const* GetAddressOf() const noexcept { return &this->p; }
+        T** GetAddressOf() noexcept
+        {
+            assert(this->p == nullptr);
+            return &this->p;
+        }
+
+        T** ReleaseAndGetAddressOf() noexcept
+        {
+            this->Release();
+            return &this->p;
+        }
+
+        void Reset() noexcept { this->Release(); }
+        explicit operator bool() const noexcept { return this->p != nullptr; }
+    };
 #endif
 // clang-format on
